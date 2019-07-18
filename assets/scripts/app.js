c7d273fd1e7a6006f7c0a580ee46b6e30d9b6657
TNBClub <47642338+TNBClub@users.noreply.github.com>
2019-07-18 21:28:40 +0430

    added slider

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,12 +4,12 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     // document.getElementById("main-header").style.fontSize = "30px";
-    document.getElementById("main-header").classList.add("scrolled");
+    document.getElementById("main--header").classList.add("scrolled");
 
 
   } else {
     // document.getElementById("main-hedear").style.fontSize = "90px";
-    document.getElementById("main-header").classList.remove("scrolled");
+    document.getElementById("main--header").classList.remove("scrolled");
 
 
   }
